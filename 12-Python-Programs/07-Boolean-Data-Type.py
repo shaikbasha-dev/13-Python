ba933c541fe4712e1252cddde83e881bdd551ce1
Shaik Mahaboob Basha <smbashadev@gmail.com>
2026-07-28 20:51:00 +0530

    feat: add boolean data type example
    - Add an example demonstrating the Boolean (bool) data type in Python.
    - Explain that Boolean values represent either True or False.
    - Demonstrate the creation and usage of Boolean variables.
    - Illustrate how to display Boolean values using the print() function.
    - Demonstrate the use of the built-in type() function to identify the Boolean data type.
    - Mention the importance of Boolean values in conditional statements, loops, and logical operations.
    - Include detailed comments and inline output explanations for beginner-friendly learning.

---
+++ 12-Python-Programs/07-Boolean-Data-Type.py
@@ -0,0 +1,61 @@
+"""
+===============================================================================
+                           Boolean Data Type
+===============================================================================
+
+Program Description:
+--------------------
+This program demonstrates the Boolean (bool) data type in Python.
+
+A Boolean data type represents one of two possible values:
+1. True
+2. False
+
+Boolean values are commonly used in decision-making statements such as
+if, if-else, while loops, and logical operations.
+
+Author      : Shaik Mahaboob Basha
+Repository  : 13-Python
+File Name   : 07-Boolean-Data-Type.py
+
+===============================================================================
+"""
+
+# -----------------------------------------------------------------------------
+# Creating a Boolean variable named status1.
+# The variable stores the Boolean value True.
+# -----------------------------------------------------------------------------
+status1 = True
+# status1 contains the Boolean value True.
+
+# -----------------------------------------------------------------------------
+# Creating a Boolean variable named status2.
+# The variable stores the Boolean value False.
+# -----------------------------------------------------------------------------
+status2 = False
+# status2 contains the Boolean value False.
+
+# -----------------------------------------------------------------------------
+# Displaying the value stored in status1.
+# -----------------------------------------------------------------------------
+print("Value of status1 :", status1)
+# Output: Value of status1 : True
+
+# -----------------------------------------------------------------------------
+# Displaying the value stored in status2.
+# -----------------------------------------------------------------------------
+print("Value of status2 :", status2)
+# Output: Value of status2 : False
+
+# -----------------------------------------------------------------------------
+# Displaying the data type of status1.
+# type() is a built-in function used to identify the data type of a variable.
+# -----------------------------------------------------------------------------
+print("Data Type of status1 :", type(status1))
+# Output: Data Type of status1 : <class 'bool'>
+
+# -----------------------------------------------------------------------------
+# Displaying the data type of status2.
+# -----------------------------------------------------------------------------
+print("Data Type of status2 :", type(status2))
+# Output: Data Type of status2 : <class 'bool'>
