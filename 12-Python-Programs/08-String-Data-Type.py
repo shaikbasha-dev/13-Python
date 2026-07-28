b7c3c9a1514f583bbb72953e19506a92c2b4570b
Shaik Mahaboob Basha <smbashadev@gmail.com>
2026-07-28 20:51:47 +0530

    feat: add string data type example
    - Add an example demonstrating the String (str) data type in Python.
    - Explain that strings are sequences of characters used to store textual information.
    - Demonstrate the creation of string variables using meaningful examples.
    - Illustrate how to display string values using the print() function.
    - Demonstrate the use of the built-in type() function to identify the string data type.
    - Include detailed comments and inline output explanations for beginner-friendly learning.
    - Help learners understand one of the most commonly used data types in Python programming.

---
+++ 12-Python-Programs/08-String-Data-Type.py
@@ -0,0 +1,79 @@
+"""
+===============================================================================
+                            String Data Type
+===============================================================================
+
+Program Description:
+--------------------
+This program demonstrates the String (str) data type in Python.
+
+A string is a sequence of characters enclosed within single quotes (' '),
+double quotes (" "), or triple quotes (''' ''' / """ """).
+
+Strings are used to store textual data such as names, addresses,
+sentences, and messages.
+
+Author      : Shaik Mahaboob Basha
+Repository  : 13-Python
+File Name   : 08-String-Data-Type.py
+
+===============================================================================
+"""
+
+# -----------------------------------------------------------------------------
+# Creating a string variable named student_name.
+# The variable stores the name of a student.
+# -----------------------------------------------------------------------------
+student_name = "Shaik Mahaboob Basha"
+# student_name contains the string "Shaik Mahaboob Basha".
+
+# -----------------------------------------------------------------------------
+# Creating a string variable named course.
+# The variable stores the course name.
+# -----------------------------------------------------------------------------
+course = "Python Programming"
+# course contains the string "Python Programming".
+
+# -----------------------------------------------------------------------------
+# Creating a string variable named college.
+# The variable stores the college name.
+# -----------------------------------------------------------------------------
+college = "XYZ Engineering College"
+# college contains the string "XYZ Engineering College".
+
+# -----------------------------------------------------------------------------
+# Displaying the value stored in student_name.
+# -----------------------------------------------------------------------------
+print("Student Name :", student_name)
+# Output: Student Name : Shaik Mahaboob Basha
+
+# -----------------------------------------------------------------------------
+# Displaying the value stored in course.
+# -----------------------------------------------------------------------------
+print("Course :", course)
+# Output: Course : Python Programming
+
+# -----------------------------------------------------------------------------
+# Displaying the value stored in college.
+# -----------------------------------------------------------------------------
+print("College :", college)
+# Output: College : XYZ Engineering College
+
+# -----------------------------------------------------------------------------
+# Displaying the data type of student_name.
+# type() is a built-in function used to identify the data type of a variable.
+# -----------------------------------------------------------------------------
+print("Data Type of student_name :", type(student_name))
+# Output: Data Type of student_name : <class 'str'>
+
+# -----------------------------------------------------------------------------
+# Displaying the data type of course.
+# -----------------------------------------------------------------------------
+print("Data Type of course :", type(course))
+# Output: Data Type of course : <class 'str'>
+
+# -----------------------------------------------------------------------------
+# Displaying the data type of college.
+# -----------------------------------------------------------------------------
+print("Data Type of college :", type(college))
+# Output: Data Type of college : <class 'str'>
